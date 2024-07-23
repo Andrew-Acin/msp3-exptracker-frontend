1314600ad73895908aab4870d279c852be2391f4
Andrew Acin <andrew_acin@hotmail.com>
2024-07-23 12:57:39 -0700

    added env and dockerfile

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import logo from './logo1.png'
 
 function Logo() {
     return (
-        <img src={logo} className="img-thumbnail logo-home" alt="logo image" />
+        <img src={logo} className="img-thumbnail logo-home" alt="application logo" />
     )
 }
 
